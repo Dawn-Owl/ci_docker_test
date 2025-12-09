2e43ca2f0aecf88ee9c298679cc3df4dffe68c84
hyosub <hyosub@dxr-ai.com>
2025-12-09 14:15:14 +0900

    Fix import path for app module in tests

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,10 @@
-# tests/test_basic.py
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 from app import do_something
+
 
 def test_do_something_returns_msg():
     msg = do_something("Jack")
